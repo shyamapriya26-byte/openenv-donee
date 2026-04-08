e6f6bfd1b4305063edb84cce83742ee8e421e9f4
shyamapriya26-byte <shyamapriya26@gmail.com>
2026-04-08 21:43:58 +0530

    Delete sever/main.py

--- sever/main.py
+++
@@ -1,8 +0,0 @@
-# This just imports your existing FastAPI app and re-exports it
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from app import app
-
-# The OpenEnv validator will look for this
