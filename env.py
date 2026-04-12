d714fde493aec8686181118d155b616b045e7ed4
shyamapriya26-byte <shyamapriya26@gmail.com>
2026-04-12 13:24:11 +0530

    final upload

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import random
 from models import Observation
-from tasks import TASK_DIFFICULTY_MAP, ISSUES   # changed import
+from tasks import TASK_DIFFICULTY_MAP, ISSUES
 from grader import grade_action
 
 class SupportEnv:
@@ -11,7 +11,7 @@ class SupportEnv:
         self.steps_taken = 0
 
     def reset(self, difficulty="easy"):
-        self.current_issue = random.choice(TASK_DIFFICULTY_MAP[difficulty])   # changed
+        self.current_issue = random.choice(TASK_DIFFICULTY_MAP[difficulty])
         self.current_path = random.choice(ISSUES[self.current_issue])
         self.current_step = 0
         self.steps_taken = 0
